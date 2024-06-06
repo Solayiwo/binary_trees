5a6e19457a179538a3f8a77e9b1ae8ffca67c4b8
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-06-06 00:06:02 -0700

    Is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Entry point
+ * Description: Function that checks if a node is a leaf
+ * @node: Pointer to the node to check
+ *
+ * Return: 0 if node is NULL
+*/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+	       return (1);
+	
+	return (0);	
+}
